11bafe5180737c02ca824ab306e09f579e4ca3a5
satohnorio <satohnorio@gmail.com>
2015-10-31 18:37:33 +0900

    新規作成

---
+++ WpfPrintSample/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Printing;
+
+namespace WpfPrintSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // ------------------------------------------------------------------------------------------------------------
+        #region コンストラクタ
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+        // ------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// 表示用データを管理します。
+        /// </summary>
+        private ReportSample FReport = new ReportSample();
+
+        /// <summary>
+        /// 表示用データを取得します。
+        /// </summary>
+        public ICollection<Person> List
+        {
+            get
+            {
+                return this.FReport.List;
+            }
+        }
+
+        /// <summary>
+        /// PrintDialogから印刷ボタンのクリックイベントを処理します。
+        /// </summary>
+        /// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
+        /// <param name="e">イベント引数を指定します。</param>
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            this.PrintDocument();
+        }
+
+        /// <summary>
+        /// XpsDocumentWriterから印刷ボタンのクリックイベントを処理します。
+        /// </summary>
+        /// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
+        /// <param name="e">イベント引数を指定します。</param>
+        private void xpsPrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.PrintXpsDocument();
+        }
+
+        /// <summary>
+        /// PrintDialogを使用した印刷を行います。
+        /// </summary>
+        private void PrintDocument()
+        {
+            var dlg = new PrintDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                var page = new FixedPage();
+                page.Children.Add(new SampleReport());
+
+                var cont = new PageContent();
+                cont.Child = page;
+
+                var doc = new FixedDocument();
+                doc.Pages.Add(cont);
+
+                dlg.PrintDocument(doc.DocumentPaginator, "Print1");
+            }
+        }
+
+        /// <summary>
+        /// XpsDocumentWriterを使用した印刷を行います。
+        /// </summary>
+        private void PrintXpsDocument()
+        {
+            var page = new FixedPage();
+            page.Children.Add(new SampleReport());
+
+            var cont = new PageContent();
+            cont.Child = page;
+
+            var doc = new FixedDocument();
+            doc.Pages.Add(cont);
+
+            PrintDocumentImageableArea area = null;
+            XpsDocumentWriter xps = PrintQueue.CreateXpsDocumentWriter(ref area);
+
+            xps.Write(doc.DocumentPaginator);
+        }
+    }
+}
---
+++ WpfPrintSample/Report.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrintSample
+{
+    /// <summary>
+    /// レポートに出力するサンプルデータを管理するクラスを定義します。
+    /// </summary>
+    public class ReportSample
+    {
+        // ------------------------------------------------------------------------------------------------------------
+        #region コンストラクタ
+
+        /// <summary>
+        /// MicrosoftReportSample.ReportSample クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        public ReportSample()
+        {
+            var list = this.FList;
+            list.Add(new Person("健太", "前田", new DateTime(1988, 4, 11)));
+            list.Add(new Person("優也", "福井", new DateTime(1988, 2, 8)));
+            list.Add(new Person("大地", "大瀬良", new DateTime(1991, 6, 17)));
+            list.Add(new Person("博樹", "黒田", new DateTime(1975, 2, 10)));
+            list.Add(new Person("猛", "今村", new DateTime(1991, 4, 17)));
+            list.Add(new Person("哲也", "小窪", new DateTime(1985, 4, 12)));
+            list.Add(new Person("涼介", "菊池", new DateTime(1990, 3, 11)));
+        }
+
+        #endregion
+        // ------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// 個人情報のリストを管理します。
+        /// </summary>
+        private List<Person> FList = new List<Person>();
+
+        /// <summary>
+        /// 個人情報の一覧を取得します。
+        /// </summary>
+        public ICollection<Person> List
+        {
+            get
+            {
+                return this.FList;
+            }
+        }
+    }
+
+    /// <summary>
+    /// レポートに出力する個人情報を管理するクラスを定義します。
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// 名前を設定／取得します。
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// 苗字を設定／取得します。
+        /// </summary>
+        public string FamilyName { get; set; }
+
+        /// <summary>
+        /// 年齢を設定／取得します。
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// 誕生日を設定／取得します。
+        /// </summary>
+        public DateTime BirthDay { get; set; }
+
+        // ------------------------------------------------------------------------------------------------------------
+        #region コンストラクタ
+
+        /// <summary>
+        /// MicrosoftReportSample.ReportSample クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        public Person(string firstName, string familyName, DateTime birthday)
+        {
+            this.FirstName = firstName;
+            this.FamilyName = familyName;
+            var time = DateTime.Now - birthday;
+            var date = new DateTime(time.Ticks);
+            this.Age = date.Year - 1;
+            this.BirthDay = birthday;
+        }
+
+        #endregion
+        // ------------------------------------------------------------------------------------------------------------
+
+    }
+}
