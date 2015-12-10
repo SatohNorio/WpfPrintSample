6112970483a7b692c07f87c57bdd0128787a05fe
satohnorio <satohnorio@gmail.com>
2015-12-10 17:56:39 +0900

    印刷ダイアログを出さずに既定のプリンタで画面のグリッドコントロールを直接印刷するサンプルを追加。

--- WpfPrintSample/MainWindow.xaml.cs
+++ WpfPrintSample/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,97 +18,116 @@ using System.Printing;
 
 namespace WpfPrintSample
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        // ------------------------------------------------------------------------------------------------------------
-        #region コンストラクタ
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		// ------------------------------------------------------------------------------------------------------------
+		#region コンストラクタ
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        #endregion
-        // ------------------------------------------------------------------------------------------------------------
+		#endregion
+		// ------------------------------------------------------------------------------------------------------------
 
-        /// <summary>
-        /// 表示用データを管理します。
-        /// </summary>
-        private ReportSample FReport = new ReportSample();
+		/// <summary>
+		/// 表示用データを管理します。
+		/// </summary>
+		private ReportSample FReport = new ReportSample();
 
-        /// <summary>
-        /// 表示用データを取得します。
-        /// </summary>
-        public ICollection<Person> List
-        {
-            get
-            {
-                return this.FReport.List;
-            }
-        }
+		/// <summary>
+		/// 表示用データを取得します。
+		/// </summary>
+		public ICollection<Person> List
+		{
+			get
+			{
+				return this.FReport.List;
+			}
+		}
 
-        /// <summary>
-        /// PrintDialogから印刷ボタンのクリックイベントを処理します。
-        /// </summary>
-        /// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
-        /// <param name="e">イベント引数を指定します。</param>
-        private void buttonClick(object sender, RoutedEventArgs e)
-        {
-            this.PrintDocument();
-        }
+		/// <summary>
+		/// PrintDialogから印刷ボタンのクリックイベントを処理します。
+		/// </summary>
+		/// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
+		/// <param name="e">イベント引数を指定します。</param>
+		private void buttonClick(object sender, RoutedEventArgs e)
+		{
+			this.PrintDocument();
+		}
 
-        /// <summary>
-        /// XpsDocumentWriterから印刷ボタンのクリックイベントを処理します。
-        /// </summary>
-        /// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
-        /// <param name="e">イベント引数を指定します。</param>
-        private void xpsPrintButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.PrintXpsDocument();
-        }
+		/// <summary>
+		/// XpsDocumentWriterから印刷ボタンのクリックイベントを処理します。
+		/// </summary>
+		/// <param name="sender">イベントを送信したオブジェクトを指定します。</param>
+		/// <param name="e">イベント引数を指定します。</param>
+		private void xpsPrintButtonClick(object sender, RoutedEventArgs e)
+		{
+			this.PrintXpsDocument();
+		}
 
-        /// <summary>
-        /// PrintDialogを使用した印刷を行います。
-        /// </summary>
-        private void PrintDocument()
-        {
-            var dlg = new PrintDialog();
-            if (dlg.ShowDialog() == true)
-            {
-                var page = new FixedPage();
-                page.Children.Add(new SampleReport());
+		/// <summary>
+		/// PrintDialogを使用した印刷を行います。
+		/// </summary>
+		private void PrintDocument()
+		{
+			var dlg = new PrintDialog();
+			if (dlg.ShowDialog() == true)
+			{
+				var page = new FixedPage();
+				page.Children.Add(new SampleReport());
 
-                var cont = new PageContent();
-                cont.Child = page;
+				var cont = new PageContent();
+				cont.Child = page;
 
-                var doc = new FixedDocument();
-                doc.Pages.Add(cont);
+				var doc = new FixedDocument();
+				doc.Pages.Add(cont);
 
-                dlg.PrintDocument(doc.DocumentPaginator, "Print1");
-            }
-        }
+				dlg.PrintDocument(doc.DocumentPaginator, "Print1");
+			}
+		}
 
-        /// <summary>
-        /// XpsDocumentWriterを使用した印刷を行います。
-        /// </summary>
-        private void PrintXpsDocument()
-        {
-            var page = new FixedPage();
-            page.Children.Add(new SampleReport());
+		/// <summary>
+		/// XpsDocumentWriterを使用した印刷を行います。
+		/// </summary>
+		private void PrintXpsDocument()
+		{
+			var page = new FixedPage();
+			page.Children.Add(new SampleReport());
 
-            var cont = new PageContent();
-            cont.Child = page;
+			var cont = new PageContent();
+			cont.Child = page;
 
-            var doc = new FixedDocument();
-            doc.Pages.Add(cont);
+			var doc = new FixedDocument();
+			doc.Pages.Add(cont);
 
-            PrintDocumentImageableArea area = null;
-            XpsDocumentWriter xps = PrintQueue.CreateXpsDocumentWriter(ref area);
+			PrintDocumentImageableArea area = null;
+			XpsDocumentWriter xps = PrintQueue.CreateXpsDocumentWriter(ref area);
 
-            xps.Write(doc.DocumentPaginator);
-        }
-    }
+			xps.Write(doc.DocumentPaginator);
+		}
+
+		/// <summary>
+		/// 印刷ダイアログを出さずに既定のプリンタで画面のグリッドを印刷します。
+		/// </summary>
+		private void PrintXpsDocument2()
+		{
+			var lps = new LocalPrintServer();
+			var q = lps.DefaultPrintQueue;
+			XpsDocumentWriter xps = PrintQueue.CreateXpsDocumentWriter(q);
+
+			VisualsToXpsDocument vs = (VisualsToXpsDocument)xps.CreateVisualsCollator();
+			vs.Write(this.grid1);
+			vs.EndBatchWrite();
+		}
+
+		private void xpsPrintButtonClick2(object sender, RoutedEventArgs e)
+		{
+			this.PrintXpsDocument2();
+		}
+	}
 }
